fe78b4f43c177f7c04efa249922333c84d8f5387
KhohZongEu <khohzongeu@gmail.com>
2022-12-14 22:29:21 +0800

    dual core

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@
 #include <Firebase_ESP_Client.h>
 
 //---------------------Wifi Info-------------------------
-#define WIFI_SSID "Nemo5"
-#define WIFI_PASSWORD "Nem0123456789"
+#define WIFI_SSID "CEC"
+#define WIFI_PASSWORD "CEC_2018"
 
 //--------------------Firebase Info-------------------------
 #define API_KEY "AIzaSyAHmdwFtSJ7YgBuNHSVdR9mTFHPBYi3Ta4"
@@ -23,11 +23,13 @@ unsigned long sendDataPrevMillis = 0;
 int count = 0;
 bool signupOK = false;
 
+TaskHandle_t firebaseUpload;
+
 //-------------------------Running LED---------------------
 #define Running 2
 
 //--------------------Time switch--------------------------
-#define timeSwitch 23
+#define silentSwitch 23
 int switchState;
 int maxSittingTime;
 
@@ -44,10 +46,12 @@ bool audioPlaying;
 unsigned long timePrevMillis;
 unsigned long trickPreventionMillis;
 unsigned long motorPrevMillis;
+int motorState = LOW;
+
 
 int Seconds = 0;
 int Minutes = 0;
-int Hours = 0;
+float Hours = 0;
 
 int startTime = 0;
 
@@ -66,6 +70,7 @@ int occupancy = 0;
 int violations = 0;
 const int onlineState = 1;
 bool rst = false;
+bool silentMode;
 
 int Sensor1average = 0;
 int Sensor2average = 0;
@@ -77,65 +82,7 @@ int Sensor6average = 0;
 int buttDifference = 0;
 
 
-void setup() {
-  
-  //---------------------------Start communication----------------------------
-  Serial.begin(115200);
-  mySerial.begin(9600);
 
-  //-------------------------------Running LED--------------------------------
-  pinMode(Running, OUTPUT);
-
-  //-----------------------timeSwitch---------------------
-  pinMode(timeSwitch, INPUT_PULLUP);
-
-  //---------------------Posture Chekcing System pinMode----------------------
-  pinMode(PS1,INPUT);
-  pinMode(PS2,INPUT);
-  pinMode(PS3,INPUT);
-  pinMode(PS4,INPUT);
-  pinMode(PS5,INPUT);
-  pinMode(PS6,INPUT);
-  pinMode(Running,HIGH);
-  pinMode(motorPin,OUTPUT);
-  
-
-  //---------------------------DF Player Mini setup---------------------------
-  myMP3.begin(mySerial);
-  myMP3.volume(30);
-
-  //-----------------------------Connect to WiFi-------------------------------
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  
-  while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(300);
-  }
-  
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-
-   /* Assign the api key (required) */
-  config.api_key = API_KEY;
-
-  /* Assign the RTDB URL (required) */
-  config.database_url = DATABASE_URL;
-
-  // Assign the user sign in credentials
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-
-  fbdo.setResponseSize(4096);
-
-  // Initialize the library with the Firebase authen and config
-  Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
-  signupOK = true;
-
-}
 
 void postureCheck(){
 
@@ -181,7 +128,7 @@ void postureCheck(){
 
     audioPlaying = myMP3.isPlaying();
   
-  if ((Sensor1average > 2800) && (Sensor2average > 2900) && (buttDifference > 0) && (buttDifference   < 400) && (Sensor4average > 100) && (Sensor3average > 100) && (Sensor6average == 0) && (Sensor5average == 0)) {
+  if ((Sensor1average > 2500) && (Sensor2average > 2500) && (buttDifference > 0) && (buttDifference   < 500) && (Sensor4average > 100) && (Sensor3average > 100) && (Sensor6average <= 10) && (Sensor5average <= 10)) {
     Serial.println("posture correct");
     occupancy = true;
     States = 1;
@@ -197,8 +144,9 @@ void postureCheck(){
     occupancy = true;
     States = 3;
 
-  } else if ((Sensor1average < 200) && (Sensor2average < 100) && (Sensor3average == 0) && (Sensor4average == 0) && (Sensor5average < 50) && (Sensor6average < 50)) {
+  } else if ((Sensor1average < 400) && (Sensor2average < 300) && (Sensor3average <= 10) && (Sensor4average <= 10) && (Sensor5average < 50) && (Sensor6average < 50)) {
     Serial.println("not occupied");
+    Serial.println(WiFi.localIP());
     occupancy = false;
     States = 1;
     
@@ -215,80 +163,94 @@ void postureCheck(){
     
   }
 
-    switch(States){
-      case 1:
-      
-        break;
+     switch(States){
+       case 1:
+         break;
 
-      case 2:
-        if(audioPlaying == false){
-          myMP3.volume(30);
-          myMP3.play(2);
-          warnings++;
-          violations++;
-        }
-          
-        break;
-      case 3:
-        if(audioPlaying == false){
-          myMP3.volume(30);
-          myMP3.play(3);
-          warnings++;
-          violations++;
-        }
-        
-        break;
-      case 4:
-      
-        break;
-      case 5:
-        if(audioPlaying ==false){
-          myMP3.volume(30);
-          myMP3.play(1);
-          warnings++;
-          violations++;
-        }
-        break;
-    }
+       case 2:
+         if(audioPlaying == false){
+           myMP3.volume(30);
+           if(silentMode == false){
+              myMP3.play(2);
+              digitalWrite(motorPin, HIGH);
+              if((millis() - motorPrevMillis) > 1000){
+                digitalWrite(motorPin, LOW);
+                motorPrevMillis = millis();
+            }
+           }
+           warnings++;
+           violations++;
+         }
+         break;
+       case 3:
+         if(audioPlaying == false){
+           myMP3.volume(30);
+           if(silentMode == false){
+              myMP3.play(3);
+              digitalWrite(motorPin, HIGH);
+              if((millis() - motorPrevMillis) > 1000){
+                digitalWrite(motorPin, LOW);
+                motorPrevMillis = millis();
+            }
+           }
+           warnings++;
+           violations++;
+          }
+         break;
+       case 4:
+         break;
+       case 5:
+         if(audioPlaying ==false){
+           myMP3.volume(30);
+           if(silentMode == false){
+              myMP3.play(1);
+              digitalWrite(motorPin, HIGH);
+              if((millis() - motorPrevMillis) > 1000){
+                digitalWrite(motorPin, LOW);
+                motorPrevMillis = millis();
+            }
+           }
+           warnings++;
+           violations++;
+         }
+         break;
+     }
+ }
 
-    
-}
 
 //-----------------------Backup Vibrator Alert System------------------
 void vibratorControl(){
-  if (warnings > 5){
+  if (warnings > 6){
     for(int motor=0; motor < 10;motor++){
-      if((millis() - motorPrevMillis) > 499){
-        motorPrevMillis = millis();
-        digitalWrite(motorPin, !digitalRead(motorPin));
-      }
+       digitalWrite(motorPin,motorState);      
+       if(motorState == LOW){
+         if((millis() - motorPrevMillis) > 1000){
+           motorState = HIGH;
+           motorPrevMillis = millis();
+         }else{
+          if((millis() - motorPrevMillis) > 1000){
+             motorState = LOW;
+             motorPrevMillis = millis();
+           }
+         }
+       }
+      // digitalWrite(motorPin,HIGH);
+      // delay(500);
+      // digitalWrite(motorPin,LOW);
+      // delay(500);
     }
     warnings = 0;
   }
 }
 
 //------------------------------Sitting Time---------------------------------
-void extendedSeating(){
-  switchState = digitalRead(timeSwitch);
+void silent(){
+  switchState = digitalRead(silentSwitch);
   if(switchState == 0){
-    maxSittingTime = 35;
+    silentMode = true;
   }
   if(switchState == 1){
-    maxSittingTime = 50;
-  }
-
-  if(Minutes > maxSittingTime){
-    if(audioPlaying == false){
-      myMP3.volume(30);
-      myMP3.play(1);
-    }
-    if((occupancy == true) && ((Minutes - maxSittingTime) > 2)){
-      warnings++;
-    }else if((occupancy = false) && ((Minutes - maxSittingTime) < 2)){
-      if((Minutes - maxSittingTime) > 5){
-      Minutes = 0;
-      }
-    }
+    silentMode = false;
   }
 }
 
@@ -303,16 +265,13 @@ void Timer(){
       Minutes++;
       Seconds = 0;
     }
-    if(Minutes > 59){
-      Hours++;
-      Minutes = 0;
-    }
+    Hours = Minutes / 60;
   }
 }
 
 //--------------------------Firebase Send Data------------------------------
 
-void sendDataToFirebase(){
+void sendDataToFirebase(void * pvParameters){
   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
     Firebase.RTDB.setInt(&fbdo, "data/time/hours", Hours);
@@ -325,19 +284,85 @@ void sendDataToFirebase(){
 }
 
 
+void setup(){
+   //---------------------------Start communication----------------------------
+  Serial.begin(115200);
+  mySerial.begin(9600);
+
+  //-------------------------------Running LED--------------------------------
+  pinMode(Running, OUTPUT);
+
+  //-----------------------timeSwitch---------------------
+  pinMode(silentSwitch, INPUT_PULLUP);
+
+  //---------------------Posture Chekcing System pinMode----------------------
+  pinMode(PS1,INPUT);
+  pinMode(PS2,INPUT);
+  pinMode(PS3,INPUT);
+  pinMode(PS4,INPUT);
+  pinMode(PS5,INPUT);
+  pinMode(PS6,INPUT);
+  pinMode(Running,HIGH);
+  pinMode(motorPin,OUTPUT);
+  
+
+  //---------------------------DF Player Mini setup---------------------------
+  myMP3.begin(mySerial);
+  myMP3.volume(30);
+
+  //-----------------------------Connect to WiFi-------------------------------
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+   /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  fbdo.setResponseSize(4096);
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+  signupOK = true;
+  
+  xTaskCreatePinnedToCore(sendDataToFirebase,   
+                    "firebaseUpload",    
+                    10000,       
+                    NULL,        
+                    1,           
+                    &firebaseUpload,      
+                    0);          
+}
+
 void loop(){ 
   digitalWrite(Running,HIGH);
-    
 //--------------------------Run postureCheck() -------------------------------
-  postureCheck();
+   postureCheck();
 
 //---------------------Run Backup Vibrator Alert System-----------------------
-  vibratorControl();
+ if(switchState == true){
+   vibratorControl();
+ }
 
 //----------------------------Send Data to Firebase-----------------------------
-  sendDataToFirebase();
+  // sendDataToFirebase();
 
 //------------------------sitting Time----------------
-  extendedSeating();
+  silent();
 
 }
